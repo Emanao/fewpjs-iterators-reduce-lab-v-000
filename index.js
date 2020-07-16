a9a84c03921bc7886aa4b60bc70367e81d7daa65
Eva Navarro <emanao@me.com>
2020-07-16 00:05:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 const totalBatteries = function(){
-  return batteryBatches.reduce((accu, currentElem) => accu+currentElem);
+  console.log(batteryBatches.reduce((accu, currentElem) => accu+currentElem));
+  return ;
  }
